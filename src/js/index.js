47a9c0649401858e216fbaea10c57d5a198ab68a
sdwoo <damjang67@naver.com>
2022-08-13 18:54:55 +0900

    feat: counter-text-js

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import Counter from './module/Counter.js';
+
+export default function App() {
+  new Counter();
+}
+
+App();
---
+++ src/js/module/Counter.js
@@ -0,0 +1,47 @@
+export default class Counter {
+  constructor() {
+    this.count = 0;
+    this.$value = document.querySelector('#value');
+    this.initEventListener();
+  }
+
+  increase() {
+    if (this.count > 10) {
+      return;
+    }
+
+    this.count += 1;
+    this.$value.innerText = this.count;
+  }
+
+  decrease() {
+    if (this.count <= 0) {
+      return;
+    }
+    this.count -= 1;
+    this.$value.innerText = this.count;
+  }
+
+  reset() {
+    if (this.count === 0) {
+      return;
+    }
+
+    this.count = 0;
+    this.$value.innerText = this.count;
+  }
+
+  initEventListener() {
+    document
+      .querySelector('.increase-btn')
+      .addEventListener('click', this.increase.bind(this));
+
+    document
+      .querySelector('.decrease-btn')
+      .addEventListener('click', this.decrease.bind(this));
+
+    document
+      .querySelector('.reset-btn')
+      .addEventListener('click', this.reset.bind(this));
+  }
+}
